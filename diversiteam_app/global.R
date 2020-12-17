2a9f718f3af3aee1eee1cea52d0b8e0a8b066610
vai2fc <v.ikeshoji.orlati@gmail.com>
2020-12-17 00:40:35 -0600

    A largely-functional, cleaned-up data app.

--- diversiteam_app/global.R
+++ diversiteam_app/global.R
@@ -9,7 +9,9 @@ library(plotly)
 require(scales)
 
 acs_data <- read_csv("../data/acs_data_converted.csv") %>% 
-  drop_na(xValue)
+  drop_na(xValue) %>% 
+  subset(xValue != "Bachelor's degree or higher" & xValue != "High school graduate or higher" & dropDownSub != "18 years and over" & dropDownSub != "65 years and over" | is.na(dropDownSub))
+
 
 stat_choices <- unique(acs_data$dropDownMain)
 
